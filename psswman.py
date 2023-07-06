1856687c943b13c62e467680328fac1ab590c7a6
root <danielum17@yahoo.com>
2023-07-05 20:36:17 -0400

    updated psswman

--- psswman.py
+++ psswman.py
@@ -1,22 +1,35 @@
-mast_pwd = input("Enter password.")
+#!/usr/bin/env python3
+
+mpwd = input("What is the password?")
 
 def view():
     print("See")
+    
+    with open('pssw.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
 def add():
     name = input('ID:')
     pwd = input('Password:')
+    print("Add complete.")
 
     with open('pssw.txt', 'a') as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
-mode = input("View or Add(view, add)?Press q to quit")
+def edit():
+    print("Can't add yet.")
 
-if mode == "q":
-    break
-if mode == "view":
-    view()
-elif mode == "add":
-    add()
-else:
-    print("Invalid option.")
-    continue
+while True:
+    mode = input("View, Add or Edit (v, a, e)? Press q to quit.")
+
+    if mode == "q":
+        break
+    if mode == "v":
+        view()
+    if mode == "a":
+        add()
+    elif mode =="e":
+        edit()
+    else:
+        print("Invalid option.")
+        continue
